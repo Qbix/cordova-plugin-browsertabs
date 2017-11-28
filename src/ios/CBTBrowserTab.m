c620832694f37c63627de65a38d073666b1196a1
Igor Martsekha <igor.martsekha@gmail.com>
2017-11-28 22:10:08 +0200

    Added plugin

---
+++ src/ios/CBTBrowserTab.m
@@ -0,0 +1,99 @@
+/*! @file CBTBrowserTab.m
+    @brief Browser tab plugin for Cordova
+    @copyright
+        Copyright 2016 Google Inc. All Rights Reserved.
+    @copydetails
+        Licensed under the Apache License, Version 2.0 (the "License");
+        you may not use this file except in compliance with the License.
+        You may obtain a copy of the License at
+        http://www.apache.org/licenses/LICENSE-2.0
+        Unless required by applicable law or agreed to in writing, software
+        distributed under the License is distributed on an "AS IS" BASIS,
+        WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        See the License for the specific language governing permissions and
+        limitations under the License.
+ */
+
+#import "CBTBrowserTab.h"
+
+#import <SafariServices/SFAuthenticationSession.h>
+
+@implementation CBTBrowserTab {
+  SFSafariViewController *_safariViewController;
+  SFAuthenticationSession *_authenticationVC;
+}
+
+- (void)isAvailable:(CDVInvokedUrlCommand *)command {
+  BOOL available = ([SFSafariViewController class] != nil);
+  CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK
+                                                messageAsBool:available];
+  [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+- (void)openUrlInBrowser:(CDVInvokedUrlCommand *)command {
+    NSString *urlString = command.arguments[0];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlString]];
+    
+    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+}
+
+- (void)openUrl:(CDVInvokedUrlCommand *)command {
+  NSString *urlString = command.arguments[0];
+  NSDictionary *options = command.arguments[1];
+  if (urlString == nil) {
+    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                messageAsString:@"url can't be empty"];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    return;
+  }
+
+  NSURL *url = [NSURL URLWithString:urlString];
+  if ([SFSafariViewController class] == nil) {
+    NSString *errorMessage = @"in app browser tab not available";
+    CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
+                                                messageAsString:errorMessage];
+    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+  }
+    
+    if ([options objectForKey:@"schema"] != nil) {
+        if (@available(iOS 11.0, *)) {
+            NSString* redirectScheme = [options objectForKey:@"schema"];
+        
+            SFAuthenticationSession* authenticationVC =
+            [[SFAuthenticationSession alloc] initWithURL:url
+                                       callbackURLScheme:redirectScheme
+                                       completionHandler:^(NSURL * _Nullable callbackURL,
+                                                           NSError * _Nullable error) {
+                                           _authenticationVC = nil;
+                                           CDVPluginResult *result;
+                                           if (callbackURL) {
+                                               result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: callbackURL.absoluteString];
+                                               
+                                           } else {
+                                               result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"error"];
+                                           }
+                                           [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+                                       }];
+            _authenticationVC = authenticationVC;
+            [authenticationVC start];
+        }
+    } else {
+        _safariViewController = [[SFSafariViewController alloc] initWithURL:url];
+        [self.viewController presentViewController:_safariViewController animated:YES completion:nil];
+        
+        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+    }
+
+}
+
+- (void)close:(CDVInvokedUrlCommand *)command {
+  if (!_safariViewController) {
+    return;
+  }
+  [_safariViewController dismissViewControllerAnimated:YES completion:nil];
+  _safariViewController = nil;
+}
+
+@end
---
+++ www/browsertab.js
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var exec = require('cordova/exec');
+
+var isAvailable = function(success, error) {
+  exec(success, error, 'BrowserTab', 'isAvailable', []);
+};
+
+var openUrl = function(url, opt_error) {
+  var doNothing = function() {};
+  var error = (!opt_error) ? doNothing : opt_error;
+  exec(doNothing, error, 'BrowserTab', 'openUrl', [url]);
+};
+
+var close = function(opt_error) {
+  var doNothing = function() {};
+  var error = (!opt_error) ? doNothing : opt_error;
+  exec(doNothing, error, 'BrowserTab', 'close', []);
+};
+
+/**
+ * Opens a url inside an application tab.
+ * Note that on iOS 11 and up, this uses SFAuthenticationSession
+ * @param {String} url The url to open
+ * @param {Object} [options={}]
+ * @param {String} options.schema The scheme to switch back to your app in iOS 11
+ * @param {String} options.toolbarColor The color of the toolbar, in Android
+ * @param {Function} success
+ * @param {Function} error
+ */
+exports.openUrl = function (url, options, success, error) {
+   exports.isAvailable(function (result) {
+       if (result) {
+           exports.openUrlInTab(url, options, success, error);
+       } else {
+           exports.openUrlInBrowser(url, success, error);
+       }
+   }, function () {
+       exports.openUrlInBrowser(url, success, error);
+   });
+};
+
+/**
+ * Opens a url inside an application tab.
+ * Note that on iOS 11 and up, this uses SFAuthenticationSession
+ * @param {String} url The url to open
+ * @param {Object} [options={}]
+ * @param {String} options.schema The scheme to switch back to your app in iOS 11
+ * @param {String} options.toolbarColor The color of the toolbar, in Android
+ * @param {Function} success
+ * @param {Function} error
+ */
+exports.openUrlInTab = function(url, options, success, error) {
+   exec(success, error, 'BrowserTab', 'openUrl', [url, options]);
+};
+
+/**
+ * Opens a url in the system browser
+ * @paran {String} url The url to open
+ * @param {Function} success
+ * @param {Function} error
+ */
+exports.openUrlInBrowser = function(url, success, error) {
+   exec(success, error, 'BrowserTab', 'openUrlInBrowser', [url]);
+};
+
+/**
+ * Closes a previously opened browser tab
+ * @param {Function} success
+ * @param {Function} error
+ */
+exports.close = function(success, error) {
+   exec(success, error, 'BrowserTab', 'close', []);
+};
+
+/**
+ * Checks whether openUrlInTab is available to be used
+ * @param {Function} success
+ * @param {Function} error
+ */
+exports.isAvailable = function(success, error) {
+   exec(success, error, 'BrowserTab', 'isAvailable', []);
+};
